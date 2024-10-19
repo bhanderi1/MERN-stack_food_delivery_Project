9db933683ebba99915ebd487e7c767f9383d0aad
Shurshti Bhanderi <srushtibhanderi77@gmail.com>
2024-10-19 15:17:03 +0530

    19-10-24

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const userRoutes = express.Router()
 const {verifyToken} = require('../helper/verifytoken')
 const {upload2} = require('../helper/uploadImage')
-const { signup, signIn, userProfile, updateUser } = require('../Controller/user.controller')
+const { signup, signIn, userProfile, updateUser,deleteUser } = require('../Controller/user.controller')
 
 userRoutes.post('/signUp', upload2.single('profilImage'),signup)
 userRoutes.post('/signIn', signIn)
 userRoutes.get('/user-profile',verifyToken, userProfile)
 userRoutes.put('/update-profile',verifyToken, updateUser)
+userRoutes.delete('/logout',verifyToken, deleteUser)
 
 
 module.exports = userRoutes
